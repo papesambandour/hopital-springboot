d20fe715d5708d72a6870e74b7559b33194bcdb8
Pape Samba Ndour <papesambandour@hotmail.com>
2018-12-04 02:05:22 +0000

    deploy

--- src/main/java/diti5/com/hospital/controller/AdminController.java
+++ src/main/java/diti5/com/hospital/controller/AdminController.java
@@ -15,7 +15,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.FileStore;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -34,6 +38,7 @@ public class AdminController {
 	@Autowired
 	private RoleDAO roleDAO ;
 	private String IMG_USER = "C:\\Users\\papes\\Documents\\PAPE NDOUR\\DITI5\\JEE\\workspace intelij\\hopital\\src\\main\\resources\\static\\upload\\images\\";
+	private String IMG_DELETED = "C:\\Users\\papes\\Documents\\PAPE NDOUR\\DITI5\\JEE\\workspace intelij\\hopital\\src\\main\\resources\\static\\deleted\\images\\";
 	@RequestMapping(value="/dashbord")
 	//@ResponseBody
 	//@RequestMapping(value="/course", method = RequestMethod.GET, produces="application/json")
@@ -55,13 +60,16 @@ public class AdminController {
 		ModelAndView view = new ModelAndView("admin/users/add");
 		List<Role> roles = roleDAO.findAll();
 		List<Service> services = serviceDOA.findAll();
+        Utilisateur user = new Utilisateur();
+        user.setListeRoles(new ArrayList<Role>());
+        user.setService(new Service());
 		view.addObject("roles",roles);
 		view.addObject("services",services);
+		view.addObject("user",user);
 		return view;
 	}
 	@RequestMapping(value="/users/edit/{idUser}",method = RequestMethod.GET)
-	public ModelAndView usersEdit(@PathVariable("idUser") String idUser)
-	{
+	public ModelAndView usersEdit(@PathVariable("idUser") String idUser) throws IOException {
 		ModelAndView view = new ModelAndView("admin/users/edit");
 		Utilisateur user = userDAO.findById(Integer.parseInt(idUser)).get();
 		List<Role> roles = roleDAO.findAll();
@@ -69,83 +77,95 @@ public class AdminController {
 		view.addObject("roles",roles);
 		view.addObject("services",services);
 		view.addObject("user",user);
+		List<Integer> listEtat = new ArrayList<Integer>();
+		listEtat.add(0);listEtat.add(1);
+		Path path = Paths.get(IMG_USER + user.getImg());
+		view.addObject("listEtat",listEtat);
+		view.addObject("urlImg",user.getImg());
 		return  view ;
 	}
 	@RequestMapping(value="/users/delete/{idUser}",method = RequestMethod.GET)
 	public ModelAndView usersDelete(HttpServletResponse httpResponse,@PathVariable("idUser") String idUser) throws IOException {
 		Utilisateur user = userDAO.findById(Integer.parseInt(idUser)).get();
+        try {
+            Path actualPath = Paths.get(IMG_USER + user.getImg());
+            Path deletedPath = Paths.get(IMG_DELETED + user.getImg());
+            Files.move(actualPath,deletedPath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		userDAO.delete(user);
 		httpResponse.sendRedirect("/admin/users?deleteSuccesse=1");
 		return null;
 	}
 	@RequestMapping(value="/users/save",method = RequestMethod.POST)
-	public String usersSave(HttpServletResponse httpResponse,
-			@RequestParam(value="nom", required=true) String nom,
-			@RequestParam(value="prenom", required=true) String prenom,
-			@RequestParam(value="matricule", required=true) String matricule,
-			@RequestParam(value="enabled", required=true) String enabled,
-			@RequestParam(value="idRole", required=true) List<String> idRole,
-			@RequestParam(value="idService", required=true) String idService,
-			@RequestParam(value="password", required=true) String password,
+	public String usersSave(HttpServletResponse httpResponse,Utilisateur user,
                             @RequestParam("file") MultipartFile file) throws IOException
 	{
-        String username = matricule + nom;
-		Utilisateur uVerify = userDAO.findByMatriculeOrUsername(matricule,username);
+        user.setUsername(user.getMatricule()+user.getNom());
+		Utilisateur uVerify = userDAO.findByMatriculeOrUsername(user.getMatricule(),user.getUsername());
 		String img_uri = "";
 		if(uVerify != null)
         {
             httpResponse.sendRedirect("/admin/users/add?error=1");
             return  null;
         }
-        try {
+        if(!file.isEmpty())
+        {
+            try {
 
-            // Get the file and save it somewhere
+                // Get the file and save it somewhere
+                byte[] bytes = file.getBytes();
+                img_uri = getUniqId()+file.getOriginalFilename().split("\\.")[1];//file.getOriginalFilename();
+                Path path = Paths.get(IMG_USER + img_uri);
+                Files.write(path, bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
             byte[] bytes = file.getBytes();
-            img_uri = file.getOriginalFilename();
-            Path path = Paths.get(IMG_USER + img_uri);
-            Files.write(path, bytes);
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            img_uri = getUniqId()+".png";//file.getOriginalFilename();
+            Path path = Paths.get(IMG_USER + "userdefault.png");
+            Path pathCopy = Paths.get(IMG_USER + img_uri);
+            Files.copy(path, pathCopy);
         }
-		Utilisateur u = new Utilisateur();
-		u.setNom(nom);u.setPassword(bCrypteEncoder.encode(password));u.setMatricule(matricule);
-		u.setPrenom(prenom);u.setEnabled(Integer.parseInt(enabled));
-		u.setImg(img_uri);
-		List<Role> roles = new ArrayList<Role>();
-		for(String idR : idRole)
-		{
-			roles.add(roleDAO.findById(Integer.parseInt(idR)).get());
-		}
-		u.setUsername(username);u.setService(serviceDOA.findById(Integer.parseInt(idService)).get());
-		u.setListeRoles(roles);
-		userDAO.save(u);
+		user.setPassword(bCrypteEncoder.encode("passer"));
+		user.setImg(img_uri);user.setChanged(0);
+		user.setEnabled(1);
+		userDAO.save(user);
 		httpResponse.sendRedirect("/admin/users?addSuccess=1");
 		return null;
 
 	}
 	@RequestMapping(value="/users/update",method = RequestMethod.POST)
 	public String usersUpdate(HttpServletResponse httpResponse,
-			@RequestParam(value="id", required=true) String id,
-			@RequestParam(value="nom", required=true) String nom,
-			@RequestParam(value="prenom", required=true) String prenom,
-			@RequestParam(value="enabled", required=true) String enabled,
-			@RequestParam(value="listeRoles", required=true) List<String> idRole,
-			@RequestParam(value="service", required=true) String idService) throws IOException
+			@RequestParam(value="file") MultipartFile file,
+        @RequestParam(value = "urlImg") String urlImg,
+        Utilisateur user) throws IOException
 	{
-		Utilisateur u = userDAO.findById(Integer.parseInt(id)).get();
-		u.setNom(nom);/*u.setMatricule(matricule);*/
-		u.setPrenom(prenom);u.setEnabled(Integer.parseInt(enabled));
-		List<Role> roles = new ArrayList<Role>();
-		for(String idR : idRole)
-		{
-			roles.add(roleDAO.findById(Integer.parseInt(idR)).get());
-		}
-		/*u.setUsername(username);*/u.setService(serviceDOA.findById(Integer.parseInt(idService)).get());
-		u.setListeRoles(roles);
-		userDAO.save(u);
+        String new_img_uri = "";
+       if(!file.isEmpty())
+       {
+           try {
+               byte[] bytes = file.getBytes();
+               new_img_uri = getUniqId()+file.getOriginalFilename().split("\\.")[1];
+               Path actualPath = Paths.get(IMG_USER + urlImg);
+               Path deletedPath = Paths.get(IMG_DELETED + urlImg);
+               Path newPath = Paths.get(IMG_USER + new_img_uri);
+               Files.write(newPath, bytes);
+               Files.move(actualPath,deletedPath);
+
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+           user.setImg(new_img_uri);
+       }
+       else {
+           user.setImg(urlImg);
+       }
+		userDAO.save(user);
+       System.out.println(user);
 		httpResponse.sendRedirect("/admin/users?updateSuccess=1");
 		return null;
 	}
@@ -178,4 +198,11 @@ public class AdminController {
 		ModelAndView view = new ModelAndView("admin/profil/profil");
 		return view;
 	}
+
+	public static String getUniqId()
+    {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+        String requiredDate = df.format(new Date()).toString();
+        return  requiredDate.replaceAll("[^0-9]","")+".";
+    }
 }
